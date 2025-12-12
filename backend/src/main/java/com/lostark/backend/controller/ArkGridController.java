dc48b1151c6dd42fe61f619dbbf580507f65447a
DESKTOP-SSGJ0L4\hrahn <hrahn@jinjin.co.kr>
2025-12-12 15:14:39 +0900

    로그 추가

--- backend/src/main/java/com/lostark/backend/controller/ArkGridController.java
+++ backend/src/main/java/com/lostark/backend/controller/ArkGridController.java
@@ -3,12 +3,14 @@ package com.lostark.backend.controller;
 import com.lostark.backend.dto.ArkGridResponseDto;
 import com.lostark.backend.service.ArkGridService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/ark-grid")
 @RequiredArgsConstructor
@@ -19,6 +21,7 @@ public class ArkGridController {
     @GetMapping("/{characterName}")
     public ResponseEntity<ArkGridResponseDto> getArkGrid(@PathVariable String characterName) {
         try {
+            log.info("Fetching ark grid. characterName={}", characterName);
             ArkGridResponseDto response = arkGridService.getArkGrid(characterName);
             return ResponseEntity.ok(response);
         } catch (Exception e) {
